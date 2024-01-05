31e6cfaddab8694aaa16a516650a1cda4172a3f9
jafan-mulama <62324336+jafan-mulama@users.noreply.github.com>
2024-01-05 10:18:24 +0300

    .\angular.json

--- APIMonetisationan/src/app/login/login.component.ts
+++ APIMonetisationan/src/app/login/login.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {LoginService} from "../../services/login.service";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  loginError = false;
 
+  constructor(private loginService: LoginService) {}
+  login(): void {
+    this.loginService.login(this.username, this.password).subscribe(
+      success => {
+        if (success) {
+          // Redirect or perform actions on successful login
+          console.log('Login successful');
+        } else {
+          this.loginError = true;
+        }
+      },
+      error => {
+        console.error('An error occurred during login:', error);
+        this.loginError = true;
+      }
+    );
+  }
 }
